9fe7ec945bc79759887e8b91ef0977463f6aa108
Soul-Remix <soulremix-dev@pm.me>
2023-06-15 15:29:22 +0300

    Update TemplateString and implement ParseTemplate

--- tmplify.go
+++ tmplify.go
@@ -1,24 +1,37 @@
 package tmplify
 
 import (
-	"strings"
+	"bytes"
 	"text/template"
 )
 
+var t = template.New("base")
+
 func TemplateString(templateString string, values map[string]interface{}) (string, error) {
-	tmpl, err := template.New("template").Funcs(FuncMap).Parse(templateString)
+	tmplString, data, err := handleImports(templateString, values)
+	if err != nil {
+		return "", err
+	}
+
+	tmpl, err := t.Funcs(getHelpers(t)).Parse(tmplString)
 	if err != nil {
 		return "", err
 	}
 
 	var result string
-	buffer := &strings.Builder{}
+	buffer := bytes.NewBuffer(nil)
 
-	err = tmpl.Execute(buffer, values)
+	err = tmpl.Execute(buffer, data)
 	if err != nil {
 		return "", err
 	}
 
-	result = buffer.String()
+	result = removeEmptyLines(buffer.String())
+
 	return result, nil
+}
+
+func ParseTemplates(glob string) error {
+	_, err := t.ParseGlob(glob)
+	return err
 }
